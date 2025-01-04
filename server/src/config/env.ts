3b927f96b9ba5aa9f014c971c7b84218f5a1aa22
nishaj0 <njnishaj0@gmail.com>
2025-01-04 19:17:54 +0530

    style: update biome style,
    changed indentStyle to space from tab

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -6,15 +6,15 @@ const requiredEnvVars = ["MONGO_URI"];
 
 // check for missing environment variables
 for (const key of requiredEnvVars) {
-	if (!process.env[key]) {
-		throw new Error(`Missing required env variable: ${key}`);
-	}
+  if (!process.env[key]) {
+    throw new Error(`Missing required env variable: ${key}`);
+  }
 }
 
 const ENV = {
-	MONGO_URI: process.env.MONGO_URI || "",
-	PORT: process.env.PORT || 3000,
-	ENVIRONMENT: process.env.NODE_ENV || "development",
+  MONGO_URI: process.env.MONGO_URI || "",
+  PORT: process.env.PORT || 3000,
+  ENVIRONMENT: process.env.NODE_ENV || "development",
 };
 
 export default ENV;
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,15 +7,15 @@ const app = express();
 app.use(express.json());
 
 const startServer = async () => {
-	try {
-		logger.info("Starting server...");
-		await connectDB();
-		app.listen(ENV.PORT, () => {
-			logger.info(`Server running on port ${ENV.PORT}`);
-		});
-	} catch (error) {
-		logger.error("Failed to start server", error);
-	}
+  try {
+    logger.info("Starting server...");
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      logger.info(`Server running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    logger.error("Failed to start server", error);
+  }
 };
 
 startServer();
