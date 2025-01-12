029958a6a0e908203ea733007949af533ee73c43
nishaj0 <njnishaj0@gmail.com>
2025-01-12 22:56:48 +0530

    feat(server): created login handler

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -15,7 +15,6 @@ const handleRegister = async (req: Request, res: Response, next: NextFunction) =
 
     // check if user already exists
     const duplicateUsername = await User.findOne({ username });
-    console.log({duplicateUsername})
     if (duplicateUsername) {
       throw new ErrorResponse("Username already exists", 400);
     }
@@ -41,13 +40,57 @@ const handleRegister = async (req: Request, res: Response, next: NextFunction) =
       sameSite: "none",
       secure: ENV.ENVIRONMENT === "production",
     });
+    
+    const { password: _, ...basicUserData } = user.toObject()
 
-    res.success("User registered successfully", { user, accessToken }, 201);
+    res.success("User registered successfully", { user: basicUserData, accessToken }, 201);
   } catch (error) {
     next(error);
   }
 };
 
+const handleLogin = async (req: Request, res: Response, next: NextFunction) => {
+  const {email, password} = req.body;
+
+  try{
+    // check body fields
+    if (!email || !password) {
+      throw new ErrorResponse("Missing required fields", 400);
+    }
+
+    // check if user exists
+    const user = await User.findOne({ email})
+    if (!user){
+      throw new ErrorResponse("Invalid credentials", 400);
+    }
+
+    // check password
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword){
+      throw new ErrorResponse("Invalid credentials", 400);
+    }
+
+    // create tokens
+    const accessToken = jwtUtil.generateAccessToken(user.id, ENV.ACCESS_TOKEN_SECRET);
+    const refreshToken = jwtUtil.generateRefreshToken(user.id, ENV.REFRESH_TOKEN_SECRET);
+
+    // send response
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: ENV.ENVIRONMENT === "production",
+    });
+
+    // user data without password
+    const { password: _, ...basicUserData } = user.toObject();
+
+    res.success("User logged in successfully", { user: basicUserData, accessToken });
+  } catch(error){
+    next(error);
+  }
+}
+
 export const authController = {
   handleRegister,
+  handleLogin,
 };
--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -4,5 +4,6 @@ import { authController } from "../controllers";
 const router = express.Router();
 
 router.post("/register", authController.handleRegister);
+router.post("/login", authController.handleLogin);
 
 export default router;
