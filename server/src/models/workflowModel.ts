55ba91d8e31f7018483746431144b3130fb161d6
nishaj0 <njnishaj0@gmail.com>
2025-03-09 21:30:47 +0530

    style: fixed lint and format issues

--- server/src/models/workflowModel.ts
+++ server/src/models/workflowModel.ts
@@ -1,5 +1,5 @@
+import type { ObjectId } from "mongodb";
 import { Schema, model } from "mongoose";
-import { ObjectId } from "mongodb";
 interface Workflow {
   trigger_id: ObjectId; // trigger ref
   name: string;
@@ -7,13 +7,16 @@ interface Workflow {
   status: "active" | "inactive";
 }
 
-const workflowSchema = new Schema<Workflow>({
-  trigger_id: { type: Schema.Types.ObjectId, required: true },
-  name: { type: String, required: true },
-  description: { type: String },
-  status: { type: String, required: true, enum: ["active", "inactive"], default: "active" },
-}, {
-  timestamps: true,
-})
+const workflowSchema = new Schema<Workflow>(
+  {
+    trigger_id: { type: Schema.Types.ObjectId, required: true },
+    name: { type: String, required: true },
+    description: { type: String },
+    status: { type: String, required: true, enum: ["active", "inactive"], default: "active" },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export const Workflow = model<Workflow>("Workflow", workflowSchema);
