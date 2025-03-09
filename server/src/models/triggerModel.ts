cee4496dac091a3ebb5cf9eb1b7ed279fd3c16f7
nishaj0 <njnishaj0@gmail.com>
2025-03-09 20:51:53 +0530

    feat: workflow and trigger schema adjustments

--- server/src/models/triggerModel.ts
+++ server/src/models/triggerModel.ts
@@ -1,21 +1,18 @@
 import { Schema, model } from "mongoose";
-import { ObjectId } from "mongodb";
 
 export interface Trigger {
-  workflow_id: ObjectId; // workflow ref
-  app: "GMAIL" | "DISCORD" | "REDDIT"; // TODO: update this after creating APPs
+  app: "GMAIL" | "DISCORD" | "REDDIT" | "SCHEDULE"; // TODO: update this after creating APPs
   event: string; // event type (like "message_sent") // TODO: update this type after creating APPs, check event by selected APP
   conditions?: {
     field: string;
     operator: "equals" | "contains" | "starts_with" | "ends_with"; // TODO: update this type after creating APPs
     value: any;
   };
-  data_schema?: Record<string, any>; // the data type might be different for each app and event
+  data_schema?: Record<string, any>; // the data type might be different for each app and event, fo the SCHEDULE we will stores the cron time and timezone
 }
 
 const triggerSchema = new Schema<Trigger>(
   {
-    workflow_id: { type: Schema.Types.ObjectId, required: true },
     app: { type: String, required: true },
     event: { type: String, required: true },
     conditions: {
--- server/src/models/workflowModel.ts
+++ server/src/models/workflowModel.ts
@@ -1,12 +1,14 @@
 import { Schema, model } from "mongoose";
-
+import { ObjectId } from "mongodb";
 interface Workflow {
+  trigger_id: ObjectId; // trigger ref
   name: string;
   description?: string;
   status: "active" | "inactive";
 }
 
 const workflowSchema = new Schema<Workflow>({
+  trigger_id: { type: Schema.Types.ObjectId, required: true },
   name: { type: String, required: true },
   description: { type: String },
   status: { type: String, required: true, enum: ["active", "inactive"], default: "active" },
